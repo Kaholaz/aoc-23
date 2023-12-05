51af159532c64e2bff5f4eb1dbb15ad04242e26c
Sebastian Bugge <vkbugge@hotmail.com>
2023-12-05 12:52:01 +0100

    Improve memory usage.

--- 05/2.rs
+++ 05/2.rs
@@ -1,11 +1,10 @@
 use std::fs;
-use std::iter::zip;
 
-fn transform(dst: u32, src: u32, range: u32, old_seed: u32, default: u32) -> u32 {
+fn transform(dst: u32, src: u32, range: u32, old_seed: u32) -> Option<u32> {
     return if old_seed < src || src + range <= old_seed {
-        default
+        None
     } else {
-        old_seed - src + dst
+        Some(old_seed - src + dst)
     };
 }
 
@@ -14,24 +13,22 @@ fn main() {
     let mut lines = input.lines();
 
     let seed_str = lines.next().unwrap();
-    let mut seed_vec = vec![];
+    let mut seeds = vec![];
     let mut prev = None::<u32>;
     for u in seed_str.split(" ").filter_map(|c| c.parse::<u32>().ok()) {
         match prev {
             Some(start) => {
-                seed_vec.append(&mut (start..start + u).collect());
+                seeds.append(&mut (start..start + u).map(|u| (u, u)).collect());
                 prev = None;
             }
             None => prev = Some(u),
         }
     }
-    let mut seeds = seed_vec.into_iter();
-    let mut new_seeds: Vec<u32> = seeds.clone().collect();
 
     for (nr, line) in lines.enumerate() {
         println!("Line : {}", nr);
         if line.contains("map:") {
-            seeds = new_seeds.clone().into_iter();
+            seeds = seeds.into_iter().map(|(_, new)| (new, new)).collect();
             continue;
         }
 
@@ -42,10 +39,14 @@ fn main() {
         if mapping.len() != 3 {
             continue;
         }
-        new_seeds = zip(seeds.clone(), new_seeds)
-            .map(|(old, new)| transform(mapping[0], mapping[1], mapping[2], old, new))
+        seeds = seeds
+            .into_iter()
+            .map(|(old, new)| match transform(mapping[0], mapping[1], mapping[2], old) {
+                Some(new) => (old, new),
+                None => (old, new),
+            })
             .collect();
     }
 
-    println!("{}", new_seeds.iter().reduce(|a, b| a.min(b)).unwrap());
+    println!("{}", seeds.iter().map(|(_, u)| u).reduce(|a, b| a.min(b)).unwrap());
 }
