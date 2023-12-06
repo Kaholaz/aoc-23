57ed27414f3dfd859d20940fecd38611dc8b3c52
Sebastian Bugge <vkbugge@hotmail.com>
2023-12-06 15:18:22 +0100

    Update with correct part 1.

--- 05/1.pl
+++ 05/1.pl
@@ -4,24 +4,18 @@ use v5.34;
 use List::Util qw(min zip);
 
 sub replace {
-	my ($dst, $src, $range, $nums) = @_;
-	my ($default, $num) = @$nums;
+	my ($dst, $src, $range, $default, $num) = @_;
 	return $default if ($num < $src || $src + $range <= $num);
 	return $num - $src + $dst;
 }
 
 my $first_line = <>;
+my @new_seeds = $first_line =~ /\d+/g;
 my @seeds = ();
-my @new_seeds = ();
-my %nums = $first_line =~ /\d+/g;
-while (my ($start, $range) = each %nums) {
-	say "$start, $range";
-	push @new_seeds, ($start..$start + $range);
-}
 while (<>) {
 	if (/map:/) { @seeds = @new_seeds; next; }
 	elsif (not /(\d+) (\d+) (\d+)/) { next; }
-	@new_seeds = map { replace $1, $2, $3, $_ } zip (\@new_seeds, \@seeds);
+	@new_seeds = map { replace $1, $2, $3, @$_ } zip (\@new_seeds, \@seeds);
 }
 say min @new_seeds;
 
