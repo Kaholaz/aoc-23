3fa392d351a8ae351243452f474d0283858b754d
Sebastian Bugge <vkbugge@hotmail.com>
2023-12-09 08:27:24 +0100

    Day 9 solution.

---
+++ 09/1.pl
@@ -0,0 +1,26 @@
+use warnings;
+use strict;
+use v5.34;
+use List::Util qw(any);
+
+my $result = 0;
+while (<>) {
+	my @nums = /-?\d+/g;
+	next if (not @nums);
+
+	my @difs = (\@nums);
+	do {
+		my $prev = 0;
+		my @dif = (map { my $out = $_ - $prev; $prev = $_; $out } @{$difs[-1]});
+		@dif = @dif[1..$#dif];
+		@difs = (@difs, \@dif);
+	} while (any {$_} @{$difs[-1]});
+
+	for (my $i = scalar @difs - 2; $i >= 0; --$i) {
+		$difs[$i]->[-1] = $difs[$i]->[-1] + $difs[$i + 1]->[-1];
+	}
+	$result += $difs[0]->[-1];
+}
+
+say $result;
+
